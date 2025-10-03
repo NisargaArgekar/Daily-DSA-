ad9b6043c2fbbf00afbd561571092af810f2a477
NisargaArgekar <argekarnisarga015@gmail.com>
2025-10-03 23:49:13 +0530

    SubArray Sum Problem K

--- subarraySum.java
+++ subarraySum.java
@@ -1,6 +1,4 @@
 import java.util.*;
-
-
 public class subarraySum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -8,13 +6,22 @@ public class subarraySum {
         int[] arr=new int[n];
         int k = sc.nextInt();
 
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0,1);
 
         int currsum = 0;
         int count = 0;
 
-        for()
-
+        for(int i=0; i<n; i++){
+            currsum += arr[i];
+            if(map.containsKey(currsum-k)){
+                count+= map.get(currsum-k);
+            }
+            map.put(currsum, map.getOrDefault(currsum, 0)+1);
+        }
+        System.out.println(count);
     }
 }

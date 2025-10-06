63f54b48a9ace16d3292d22ecf5a4ec7fae48853
NisargaArgekar <argekarnisarga015@gmail.com>
2025-10-07 00:19:58 +0530

    Javascript Function declaration in 3 ways

---
+++ Javascript Practice/FunctionDeclaration.js
@@ -0,0 +1,19 @@
+//  Function Declaration
+function greet() {
+    console.log("Hello from Function declaration");
+}
+greet();
+
+
+// Function Expression
+const greetExpression = function() {
+    console.log("Hello from Function Expression!");
+}
+greetExpression();
+
+
+// Arrow Function 
+const greetArrow = () => {
+    console.log("Hello from Arrow Function!");
+}
+greetArrow();

13d5e8f9fa28f3c1b975bd0f76664efbb4695399
NisargaArgekar <argekarnisarga015@gmail.com>
2025-10-08 23:55:44 +0530

    Leap Year Checking

--- LeapYear.java
+++ LeapYear.java
@@ -1,6 +1,6 @@
 public class LeapYear {
     public static void main(String[] args) {
-        int year = 2024;
+        int year = 1996;
         if((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
         System.out.println("Leap Year");
         else 

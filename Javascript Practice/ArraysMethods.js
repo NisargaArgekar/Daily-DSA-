99ec7d10cb48bf01861aaa647e82a2cb0062d1d5
NisargaArgekar <argekarnisarga015@gmail.com>
2025-10-09 15:04:53 +0530

    Working

--- Javascript Practice/ArraysMethods.js
+++ Javascript Practice/ArraysMethods.js
@@ -1,4 +1,4 @@
-numbers = [1,5,4,3,6,7,9];
+let numbers = [1,5,4,3,6,7,9];
 console.log(numbers.indexOf(7));
 
 console.log(numbers);
@@ -7,3 +7,15 @@ console.log(numbers);
 
 console.log(numbers.reverse());
 
+let fruits = ["apple", "Mango" ,"Cherry"];
+// fruits.forEach(fruit => console.log(fruit));
+fruits.forEach(fruit => process.stdout.write(fruit + " "));
+
+// Higher order function 
+// Map 
+
+
+let num = [1, 2, 3];
+let squared = num.map(num =>num*num);
+console.log(num);
+console.log(squared);

4646e9bcc9b4461aa11c5cb044f23cfe2d640bf4
NisargaArgekar <argekarnisarga015@gmail.com>
2025-10-09 23:56:04 +0530

    Datatype in java script

--- Javascript Practice/DataTypes.js
+++ Javascript Practice/DataTypes.js
@@ -2,21 +2,40 @@
 
 // String
 let name = 'Nisa';
+console.log(name);
 
 // Number
 let num = 36;
+console.log(num);
 
 // Boolean 
 let isCoder = true;
+console.log(isCoder);
 
 // Undefined
 let x;
+console.log(x);
 
 //Null
 let y = null;
+console.log(y);
 
 // BigInt
 let bigNum = 12345n;
+console.log(bigNum);
 
 // Symbol
 let uniqueId = Symbol();
+console.log(uniqueId);
+
+// Non - Primitive DataTypes
+
+let person = {
+    name: "Nisa",
+    id: 101
+}
+
+
+console.log(typeof name);
+console.log(typeof id);
+console.log(typeof person);

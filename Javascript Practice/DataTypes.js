4aba1fd3837df864b3cfb05804496ec6b5730e06
NisargaArgekar <argekarnisarga015@gmail.com>
2025-10-09 23:51:14 +0530

    DataTypes

---
+++ Javascript Practice/DataTypes.js
@@ -0,0 +1,22 @@
+//Primitive Data Types 
+
+// String
+let name = 'Nisa';
+
+// Number
+let num = 36;
+
+// Boolean 
+let isCoder = true;
+
+// Undefined
+let x;
+
+//Null
+let y = null;
+
+// BigInt
+let bigNum = 12345n;
+
+// Symbol
+let uniqueId = Symbol();

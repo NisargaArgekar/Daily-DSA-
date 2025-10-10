1cb58cadcc72ef86bd16dd91f6f217825683c0bc
NisargaArgekar <argekarnisarga015@gmail.com>
2025-10-10 23:27:23 +0530

    Sorting Names using TreeSet

--- SortingNames.java
+++ SortingNames.java
@@ -4,13 +4,16 @@ public class SortingNames {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        sc.nextLine();
 
         TreeSet <String> names = new TreeSet<>();
+        System.out.println("Enter names: ");
         for(int i=0; i<n; i++){
             String name = sc.nextLine();
             names.add(name);
         }
-
+        
+        System.out.println("Sorted names: ");
         for(String name : names){
             System.out.println(name);
         }

fe229cf0065c9ace36a1cb5593d08380f3a99bb8
NisargaArgekar <argekarnisarga015@gmail.com>
2025-10-13 00:00:28 +0530

    Implicit Explicit

--- Javascript Practice/implicit_Explicit.js
+++ Javascript Practice/implicit_Explicit.js
@@ -9,5 +9,20 @@ let str = "123";
 let num = Number(str);
 console.log(num + 10); //Converts String to Number
 
+let boolVal = Boolean(0); // converts 0 to false
+console.log(boolVal);
+
+// Examples
+
+console.log(Number("5") + 5);
+console.log(String(5) + 5);
+console.log(Boolean("Hello"));
+console.log(Boolean(""));
+
+
+
+
+
+
 
 

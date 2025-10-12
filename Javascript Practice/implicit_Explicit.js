2dc884c047b93b9732cf72426711e123be4deff6
NisargaArgekar <argekarnisarga015@gmail.com>
2025-10-12 23:57:31 +0530

    Implicit and Explicit

---
+++ Javascript Practice/implicit_Explicit.js
@@ -0,0 +1,13 @@
+// implicit  Conversion
+console.log('5' + 5);
+console.log('5' - 2);
+console.log('10' * '2');
+
+//explicit Conversion
+
+let str = "123";
+let num = Number(str);
+console.log(num + 10); //Converts String to Number
+
+
+

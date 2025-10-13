d8e15adec05c9179e1f3fc2b2c63a1987e82faeb
NisargaArgekar <argekarnisarga015@gmail.com>
2025-10-13 14:38:51 +0530

    Constructs

--- Javascript Practice/ControlConstructer.js
+++ Javascript Practice/ControlConstructer.js
@@ -1,4 +1,5 @@
-// 
+// If Statement - A student is eligible for voting if age ≥ 18.
+
 let age = 45;
 if(age >= 18) {
     console.log("Student is eligible for Voting");
@@ -8,8 +9,7 @@ if(age >= 18) {
 console.log("Thank You!!");
 
 
-
-// If-Else for checking Positive or Negative
+//If-Else Statement - Write a program to check if a number is positive or negative.
 let a = 0;
 if(a >=0) {
     console.log("Positive Number");
@@ -18,7 +18,9 @@ else {
     console.log("Negative number");
 }
 
-// Write if - else ladder to check the number is positive, negative and zero
+
+
+//If-Else Ladder- Check if a number is positive, negative, or zero.
 let number = 77;
 if(number > 0){
     console.log("Positive Number");
@@ -27,3 +29,55 @@ if(number > 0){
 } else {
     console.log("Zero");
 }
+
+
+
+//Nested If - A student passes if marks ≥ 40. If marks ≥ 80, show “Distinction”.
+
+
+
+//Switch Case 1. Print the grade based on letter (A=Excellent, B=Good, C=Average, D=Poor).
+//2. print the season based on months in the year (3,4,5 - summer) (11,12,1,2 - winter) (6,7,8,9 - rainy)
+
+
+
+
+
+
+
+
+
+
+//---------- Looping constructs ---------------------
+
+//For Loop - Print the first 10 natural numbers.
+
+//While Loop - Print numbers from 1 to 5 using while loop.
+
+//Do-While Loop: ATM machine: Keep asking PIN until the user enters 1234.
+
+
+
+//For…of Loop - Print all fruits in a basket.
+
+//For…in - Print all student details from an object.
+
+//forEach Loop - Print the squares of all numbers in an array.
+
+
+
+
+
+
+
+
+
+// ------------------ control flow modifiers------------------------
+
+//Break Statement - Question: Stop printing when number reaches 3.
+
+//Continue Statement -Question: Skip printing number 3.
+
+
+
+

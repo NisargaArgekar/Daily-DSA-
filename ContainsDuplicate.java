ef9fa369c2aac8765416147eed612d0e22296cd2
NisargaArgekar <argekarnisarga015@gmail.com>
2025-10-13 10:08:13 +0530

    Brute force ContainsDuplicate

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -1,16 +1,22 @@
-public class ContainsDuplicate {
-    public static boolean dulicate(int[] num) {
-        boolean isTrue = true;
-        for(int i=0; i<num.length; i++){
-            for(int j=0; j<num.length; j++){
+// num = [1,2,3,4,1]
+
+// Brute Force with Time Complexity: o(n^2) and Space Complexity:o(1) 
+
+public class ContainsDuplicate{
+    public static void main(String[] args) {
+        int num[] = {1,2,3,4};
+        System.out.println(Duplicate(num));
+       
+    }
+
+    public static boolean Duplicate(int[] num){
+         for(int i=0; i<num.length; i++){
+            for(int j=i+1; j<num.length; j++){
                 if(num[i] == num[j]){
                     return true;
                 }
-                else {
-                    return false;
-                }
             }
         }
-        
+        return false;
     }
 }

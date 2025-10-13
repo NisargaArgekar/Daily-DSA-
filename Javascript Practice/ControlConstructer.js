2480557f04b5877e5eb6b2f9c70d7aaea3656d6f
NisargaArgekar <argekarnisarga015@gmail.com>
2025-10-13 14:35:43 +0530

    Control Constructor in JS

---
+++ Javascript Practice/ControlConstructer.js
@@ -0,0 +1,29 @@
+// 
+let age = 45;
+if(age >= 18) {
+    console.log("Student is eligible for Voting");
+} else {
+    console.log("Student is not eligible for Voting");
+}
+console.log("Thank You!!");
+
+
+
+// If-Else for checking Positive or Negative
+let a = 0;
+if(a >=0) {
+    console.log("Positive Number");
+}
+else {
+    console.log("Negative number");
+}
+
+// Write if - else ladder to check the number is positive, negative and zero
+let number = 77;
+if(number > 0){
+    console.log("Positive Number");
+} else if(number < 0){
+    console.log("Negative Number");
+} else {
+    console.log("Zero");
+}
